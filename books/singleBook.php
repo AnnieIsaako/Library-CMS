c8a0f5e8a1a32f3a96eccc831a4d78ed49e68a20
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-10-09 10:05:16 +1300

    Using composer, packagist, learning about vendor

--- books/singleBook.php
+++ books/singleBook.php
@@ -1,10 +1,6 @@
 <?php
-    require('templates/header.php');
-    include('templates/nav.php');
+    require('../templates/header.php');
 ?>
-
-    <div class="container">
-        <?php include('templates/links.php'); ?>
 
         <div class="row mb-2">
             <div class="col">
@@ -54,9 +50,4 @@
         </div>
     </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/script.js"></script>
-</body>
-</html>
+    <?php require('../templates/script.php'); ?>
