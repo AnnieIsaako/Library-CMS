108d92e72b50edc45e6835058deb6fbe710ab9e8
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-10-09 09:09:10 +1300

    Templates, php library

---
+++ allBooks.php
@@ -0,0 +1,11 @@
+<?php
+    require('templates/header.php');
+?>
+        <?php
+            include('templates/listingForAll.php');
+        ?>
+    </div>
+
+    <?php require('templates/script.php'); ?>
+</body>
+</html>
