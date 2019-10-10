56bdb536b5b73788f574c227d88e14ece4c8e272
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-10-11 11:36:51 +1300

    db updated, it has values now

--- 1902_library.sql
+++ 1902_library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 09, 2019 at 03:48 PM
+-- Generation Time: Oct 10, 2019 at 10:29 PM
 -- Server version: 5.7.27-0ubuntu0.16.04.1
 -- PHP Version: 7.0.33-8+ubuntu16.04.1+deb.sury.org+1
 
@@ -31,6 +31,14 @@ CREATE TABLE `authors` (
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`_id`, `name`) VALUES
+(1, 'test author'),
+(2, 'Roald Dahl');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +52,18 @@ CREATE TABLE `books` (
   `year` year(4) NOT NULL,
   `author_id` tinyint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`_id`, `title`, `description`, `year`, `author_id`) VALUES
+(1, 'test book', 'This is the description of our test book that is supposed to be great', 2002, 1),
+(2, 'Charlie and the chocolate factory', 'Hello this is a random description so dont take it seriously', 2002, 1),
+(4, 'Hello this is a test', 'Hello this is a random description so dont take it seriously', 2002, 1),
+(5, 'Testing book', 'fasdfjasfljsfl;s fsl;kjs fl;sjf sflkj ;slj lks;j f', 2001, 1),
+(6, 'asdfa', 'asdfasfasdffasfsafsadfasdfasfsafsadfsafsafd', 2002, 1),
+(7, 'asdfasdfsadf', 'asdfsadfsafdsadfsadfasfas', 2002, 1);
 
 -- --------------------------------------------------------
 
@@ -109,12 +129,12 @@ ALTER TABLE `movies`
 -- AUTO_INCREMENT for table `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `_id` tinyint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `directors`
 --
