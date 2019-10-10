7d846b4fab8573f53c11bb38c30e3fb8e09d153c
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-10-11 10:39:55 +1300

    Created sql query to submit book input

--- books/addBook.php
+++ books/addBook.php
@@ -3,7 +3,7 @@
 
 
     if($_POST) {
-        var_dump($_POST);
+        // var_dump($_POST);
         // var_dump($_POST['title']);
         // var_dump('You have submitted a book');
 
@@ -44,7 +44,7 @@
 
         if (empty($description)) {
             array_push($errors, 'Please enter a book description');
-        } else if (strlen($description) < 50) {
+        } else if (strlen($description) < 10) {
             array_push($errors, 'The description length must be at least 10 characters');
         } else if(strlen($description) > 65535) {
             array_push($errors, 'Description must have less than 65535 characters');
@@ -52,15 +52,25 @@
 
         if(empty($errors)) {
             $safeBkTitle = mysqli_real_escape_string($dbc, $bkTitle); // safe version - turning everything into a string version
+            $safeYear = mysqli_real_escape_string($dbc, $year);
             $safeAuthor = mysqli_real_escape_string($dbc, $author);
             $safeDescription = mysqli_real_escape_string($dbc, $description);
 
-            $sql = "INSERT INTO `authors`(`name`) VALUES ('$author')";
-            $result = mysqli_query($dbc, $sql);
-            if($result && mysqli_affected_rows($dbc) > 0) {
-                var_dump('author was added');
+            // $sql = "INSERT INTO `authors`(`name`) VALUES ('$author')";
+            // $result = mysqli_query($dbc, $sql);
+            // if($result && mysqli_affected_rows($dbc) > 0) {
+            //     $authorID = $dbc->insert_id; // gives us the id of last inserted row
+            // } else {
+            //     die('couldnt add author');
+            // }
+            $authorID = 1;
+
+            $booksSql= "INSERT INTO `books`(`title`, `description`, `year`, `author_id`) VALUES ('$safeBkTitle', '$safeDescription', $safeYear, $authorID)";
+            $booksResult = mysqli_query($dbc, $booksSql);
+            if($booksResult && mysqli_affected_rows($dbc) > 0) {
+                header('Location:singleBook.php');
             } else {
-                die('couldnt add author');
+                die('There is something wrong with your sql');
             }
         }
 
@@ -122,10 +132,4 @@
         </div>
 
     </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="./js/bootstrap.min.js"></script>
-    <script src="./js/script.js"></script>
-</body>
-</html>
+    <?php require('../templates/script.php'); ?>
